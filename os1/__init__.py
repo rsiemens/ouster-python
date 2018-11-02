0943c5702fc69367ca5a4114f0fa931fcb5f1cbc
rsiemens <ryanjsiemens@gmail.com>
2018-11-02 13:16:49 -0700

    initial commit

---
+++ os1/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+os1 is a python client for the Ouster lidar os-1.
+"""
+
+__author__ = "Ryan Siemens"
+__email__ = "ryanjsiemens@gmail.com"
+__version__ = "0.1.0"
+__license__ = "MIT"
---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+import os1
+
+setup(
+    name='ouster-os1',
+    version=os1.__version__,
+    description=os1.__doc__,
+    author=os1.__author__,
+    url='https://github.com/rsiemens/',
+    packages=['os1'],
+)
